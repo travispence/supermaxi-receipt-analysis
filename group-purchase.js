d3a0048311f0706ea36bd6ee69338b95d6be5eea
Travis Pence <travis.pence@gmail.com>
2018-05-25 15:35:12 -0500

    Format file and remove log statements

--- group-purchase.js
+++ group-purchase.js
@@ -1,41 +1,37 @@
 const parser = require('xml2json');
-const readFile  = require('fs').readFile;
-const writeFile  = require('fs').writeFile;
+const readFile = require('fs').readFile;
+const writeFile = require('fs').writeFile;
 const readdir = require('fs').readdir;
 const dataDir = './data/'
 
 
 function groupBy(xs, key) {
-    return xs.reduce(function(rv, x) {
-      (rv[x[key]] = rv[x[key]] || []).push(x);
-      return rv;
-    }, {});
+  return xs.reduce(function (rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
 
 }
-readdir('data/', function(err, files) {
-  if( err ) {
-    console.error( "Could not list the directory.", err );
-    process.exit( 1 );
+readdir(dataDir, function (err, files) {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(-1);
   }
-  var promises = files.map( function( file, index ) {
+  const promises = files.map(function (file, index) {
     return new Promise(function (resolve, reject) {
-      readFile('data/' + file, 'utf8', function(err, xmlData) {
-       let jsonStr = parser.toJson(xmlData);
-       let recieptXML = JSON.parse(jsonStr).autorizacion.comprobante;
-       let recieptObject = recieptXML
+      readFile(dataDir + file, 'utf8', function (err, xmlData) {
+        const jsonStr = parser.toJson(xmlData);
+        const recieptXML = JSON.parse(jsonStr).autorizacion.comprobante;
+        const recieptObject = recieptXML
 
         if (err) {
-        console.error(err);
-         process.exit(-1);
+          console.error(err);
+          process.exit(-1);
         }
-        console.log( 'data / ' + file)
 
+        const items = recieptObject.factura.detalles.detalle;
 
-        let items = recieptObject.factura.detalles.detalle;
-        let recieptDetail = recieptObject.factura.infoFactura
-        // delete recieptDetail.pagos
-
-        let purchasedItems = items.map( i => {
+        const purchasedItems = items.map(i => {
           i.impuestos = i.impuestos.impuesto
           return i;
         });
@@ -47,23 +43,23 @@ readdir('data/', function(err, files) {
   Promise.all(promises)
     .then(function (data) {
       // flatten data
-      data = data.reduce(function(a, b) {
+      data = data.reduce(function (a, b) {
         return a.concat(b);
       });
 
-
       const grouped = groupBy(data, 'descripcion');
-      const count = Object.keys(grouped).map( function (k) {
+      const count = Object.keys(grouped).map(function (k) {
         return {
           item: k,
           count: grouped[k].length,
-          prices:  grouped[k].map(p => parseFloat(p.precioUnitario)),
-          averageCost : grouped[k].map(p => parseFloat(p.precioUnitario)).reduce(function(a, b){ return a + b}, 0) / grouped[k].length
+          prices: grouped[k].map(p => parseFloat(p.precioUnitario)),
+          averageCost: grouped[k].map(p => parseFloat(p.precioUnitario)).reduce(function (a, b) {
+            return a + b
+          }, 0) / grouped[k].length
         }
       });
 
       console.log(JSON.stringify(count, null, 2))
-      // console.log(JSON.stringify(grouped, null, 2))
       process.exit(0)
     })
     .catch(function (error) {
